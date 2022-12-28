437197e28369284696101be650b69c2ac7933d67
Jalyn Kyrgyz <87895714+jalynkyrgyz@users.noreply.github.com>
2022-12-28 17:32:44 +0600

    users API done / axios installed 51 study

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,59 +1,25 @@
 /* eslint-disable jsx-a11y/alt-text */
+import axios from 'axios'
 import React from 'react'
 import styles from './Users.module.css'
+import userDefaultPhoto from '../../assets/images/defaultImage.png'
 
-const Users = (props) => { 
-     
-    if (props.users.length === 0) {
-        props.setUsers( 
-            [
-                {   
-                    id:1, 
-                    photoUrl: 'https://static.kinoafisha.info/k/persons/1080x1920/upload/persons/358732453407.jpg',
-                    followed: true, 
-                    fullName: 'Dmitry K ', 
-                    status: 'I am a boss', 
-                    location: {
-                        city: 'Minsk', 
-                        country: 'Belarus'
-                    }
-                },
-                {
-                    id:2, 
-                    photoUrl: 'https://static.kinoafisha.info/k/persons/1080x1920/upload/persons/358732453407.jpg', 
-                    followed: false, 
-                    fullName: 'Anrew B ', 
-                    status: 'I am a boss, too', 
-                    location: {
-                        city: 'Moscow', 
-                        country: 'Russia'
-                    }
-                },
-                {
-                    id:3, 
-                    photoUrl: 'https://static.kinoafisha.info/k/persons/1080x1920/upload/persons/358732453407.jpg', 
-                    followed: true, 
-                    fullName: 'Sasha G ', 
-                    status: 'I am a boss, three', 
-                    location: {
-                        city: 'Kyev', 
-                        country: 'Ukraine'
-                    }
-                }
-            ]
-        )
+const Users = (props) => {       
+    if (props.users.length === 0) {       
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        .then(response => {        
+            props.setUsers(response.data.items)
+        })       
     }
     
     
-  return (
-
-    
-  <div className={styles.users}>
+  return (   
+  <div className={styles.users}>   
                 {
                 props.users.map(user => <div key={user.id} className={styles.userItem} >
                     <span className={styles.follow}>
                         <div>
-                            <img src={user.photoUrl} className={styles.userPhoto} />
+                            <img src={user.photos.small != null ? user.photos.small : userDefaultPhoto } className={styles.userPhoto} />
                         </div>
                         <div>
                             {user.followed 
@@ -63,12 +29,12 @@ const Users = (props) => {
                     </span>
                     <span className={styles.userDetail}>
                         <span>
-                            <div>{user.fullName}</div>
+                            <div>{user.name}</div>
                             <div>{user.status}</div>
                         </span>
                         <span>
-                            <div>{user.location.country}</div>
-                            <div>{user.location.city}</div>                        
+                            <div>{'user.location.country'}</div>
+                            <div>{'user.location.city'}</div>                        
                         </span>
                     </span>
                 </div>)
