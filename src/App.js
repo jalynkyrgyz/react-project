a4692c15416e96167d7c1bf4cb22bca9ead6fa56
Jalyn Kyrgyz <87895714+jalynkyrgyz@users.noreply.github.com>
2023-01-12 17:18:42 +0600

    61 Auth added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './App.css';
-import Header from './components/Header/Header'
 import Sidebar from './components/Sidebar/Sidebar'
 import { Routes, Route } from 'react-router-dom';
 import News from './components/News/News';
@@ -8,13 +7,14 @@ import Friends from './components/Friends/Friends';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
 
 const  App = (props) => {
 
   return (
     // <Router>
       <div className="app-wrapper">
-        <Header/>
+        <HeaderContainer/>
         <Sidebar/>
         <div className='app-wrapper-content'>
           <Routes>            
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import styles from './Header.module.css'
+import { NavLink } from 'react-router-dom'
 
-const Header = () => {
+const Header = (props) => {
   return (
       <header className={styles.header}> 
         <div className={styles.header_setting}>
           <img src="https://www.freelogodesign.org/Content/img/logo-ex-7.png" alt="" />
           
-            <h3>I am header</h3>
+            <div className={styles.login_block}>
+              {props.isAuth ? props.login : <NavLink to = '/login'>login
+</NavLink> }
+              
+            </div>
         </div>
           
       </header>
