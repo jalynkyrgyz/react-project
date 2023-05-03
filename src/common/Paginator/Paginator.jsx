58d0dfe1292c9c4d739d40605f6d23085981c153
Jalyn Kyrgyz <87895714+jalynkyrgyz@users.noreply.github.com>
2023-05-03 22:58:29 +0600

    paginator added

--- src/common/Paginator/Paginator.jsx
+++ src/common/Paginator/Paginator.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Paginator.module.css'
+import cn from "classnames"
 
-const Paginator = ({totalUsersCount, pageSize, currentPage, onPageChanged }) => {
-    let pagesCount = Math.ceil(totalUsersCount / pageSize)
+const Paginator = ({totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 10 }) => {
+    let pagesCount = Math.ceil(totalItemsCount / pageSize)
         let pages = []
-        for (let i = 1; i < pagesCount; i++) {
+        for (let i = 1; i <= pagesCount; i++) {
             pages.push(i)
         }
-        let curP = currentPage
+
+        let portionCount = Math.ceil(pagesCount / portionSize)
+        let [portionNumber, setPortionNumber] = useState(1)
+        let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1
+        let rightPortionPageNumber = portionNumber * portionSize 
+
+        return (
+            <div className={styles.paginator}>
+                { portionNumber > 1 && <button onClick={()=>{setPortionNumber(portionNumber -1)}} >PREV</button>}
+                    {pages
+                        .filter(p=> p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+                        .map((p)=> {
+                        return <span className={cn({[styles.selectedPage] : currentPage === p}, styles.pageNumber)} 
+                         key={p} onClick={(e)=> {onPageChanged(p)}}>{p}</span>
+                        })
+                    }
+                {portionCount > portionNumber && <button onClick={()=> {setPortionNumber(portionNumber + 1)}}> NEXT </button>}
+            </div>
+        )
+
+
+        /*let curP = currentPage
         let curPF = ((curP - 5) ? 0 : curP - 5)
         let curPL = curP + 5
-        let slicedPages = pages.slice(curPF, curPL)
+        let slicedPages = pages.slice(curPF, curPL)     
         
   return (
     <div className={styles.pages}>  
@@ -19,7 +41,7 @@ const Paginator = ({totalUsersCount, pageSize, currentPage, onPageChanged }) => 
              key={page} onClick={(e)=> {onPageChanged(page)}} >{page}</span>
         })}         
     </div>  
-  )
+  )*/
 }
 
 export default Paginator
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import styles from './Users.module.css'
-// import userPhoto from '../../assets/images/defaultImage.png'
-// import { NavLink } from 'react-router-dom'
 import Paginator from '../../common/Paginator/Paginator'
 import User from './User'
-
-
-
 
 const Users = ({ currentPage, totalUsersCount, pageSize, onPageChanged, users, ...props}) => {
   return (
     <div >  
-        <Paginator currentPage={currentPage} onPageChanged={onPageChanged} 
-                   totalUsersCount={totalUsersCount} pageSize={pageSize} />
+        <Paginator currentPage={currentPage} onPageChanged={onPageChanged} totalItemsCount={totalUsersCount} pageSize={pageSize} />
         <div>
             {
                 users.map(user => <User key ={user.id} user={user}
@@ -22,37 +16,7 @@ const Users = ({ currentPage, totalUsersCount, pageSize, onPageChanged, users, .
                                         className={styles.userItem}      
                                   />)
             }
-        </div>
-
-        {/* {           
-           users.map(user => <div key={user.id} className={styles.userItem} >
-                <span className={styles.follow}>
-                    <div>
-                        <NavLink to={'/profile/' + user.id}  >
-                            <img src={user.photos.small != null ? user.photos.small : userPhoto } className={styles.userPhoto} alt="" />
-                        </NavLink>                               
-                    </div>
-                    <div>
-                        {user.followed 
-                         ? <button className={styles.btn} disabled={props.followingInProgress.some(id=>id === user.id)} 
-                                    onClick={()=>{ props.unfollow(user.id) }} >Unfollow</button> 
-                                    : <button className={styles.btn} disabled={props.followingInProgress.some(id=>id === user.id)}
-                                      onClick={()=>{props.follow(user.id) }}>Follow</button>
-                        }                        
-                    </div>
-                </span>
-                <span className={styles.userDetail}>
-                    <span>
-                        <div>{user.name}</div>
-                        <div>{user.status}</div>
-                    </span>
-                    <span>
-                        <div>{'user.location.country'}</div>
-                        <div>{'user.location.city'}</div>                        
-                    </span>
-                </span>
-            </div>)
-        }  */}
+        </div>      
 
     </div>  
   )
